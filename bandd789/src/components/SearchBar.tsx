cafbd5701dc2c1d16c321e274b7fe980b1f4511c
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-08-18 11:34:45 +0700

    Delete bandd789 directory

--- bandd789/src/components/SearchBar.tsx
+++
@@ -1,5 +0,0 @@
-export default function SearchBar({ q, setQ }: { q: string, setQ: (v:string)=>void }){
-  return (
-    <input className="input" placeholder="ค้นหาแบบบ้าน เช่น Minimal, 3 ห้องนอน..." value={q} onChange={e=>setQ(e.target.value)} />
-  )
-}
