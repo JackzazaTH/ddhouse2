8eae658b951bd79e0e724b393d676b304010c91e
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-08-18 12:01:20 +0700

    Delete BANDD789 directory

--- BANDD789/src/components/Skeleton.jsx
+++
@@ -1,3 +0,0 @@
-export default function Skeleton({ h=14, w='100%' }){
-  return <div style={{height:h, width:w, background:'linear-gradient(90deg,#eee,#f6f6f6,#eee)', backgroundSize:'200% 100%', animation:'s 1.2s infinite', borderRadius:8}}/>
-}
